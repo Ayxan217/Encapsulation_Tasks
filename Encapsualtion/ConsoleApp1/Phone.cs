b9c3cc8b1b3ce9a0703bca090cbc9321e48c88d2
ayxan217 <ayxanrm-bp217@code.edu.az>
2024-10-16 11:29:25 +0400

    part 2 added

---
+++ Encapsualtion/ConsoleApp1/Phone.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Encapsulation;
+
+public class Phone : Product
+{
+    
+    private int _ram;
+    private string _color;
+    private decimal _balance;
+
+    public Phone(string brandName, string model, int price, int cost, int count, int ram, string color, decimal balance) : base(brandName, model, price, cost, count)
+    {
+        Ram = ram;
+        Color = "black";
+        Balance = balance;
+
+
+
+    }
+   public decimal Call(int seconds, decimal balance)
+    {
+        if (balance <= 0)
+        {
+            Console.WriteLine("balansınızı artırın ");
+        }
+        else
+        {
+
+        Console.WriteLine("zeng edildi");
+            for (int i = 1; i <= seconds; i++)
+            {
+                balance -= 0.1m;
+            if (balance <= 0)
+            {
+                Console.WriteLine($"{i} ci saniyede balansınız bitdi ");
+                    break;
+                
+            }
+            }
+               
+        }
+        return balance;
+    }
+
+    public int Ram
+    {
+        get { return _ram; }
+        set
+        {
+            if (value > 0) 
+            {
+                _ram = value;
+            }
+            else
+            {
+                Console.WriteLine("RAM must be greater than 0."); 
+            }
+        }
+    }
+
+    public string Color
+    {
+        get { return _color; }
+        set
+        {
+            if (!string.IsNullOrEmpty(value)) 
+            {
+                _color = value;
+            }
+            else
+            {
+                Console.WriteLine("Color cannot be empty."); 
+            }
+        }
+    }
+
+    public decimal Balance
+    {
+        get { return _balance; }
+        set
+        {
+            if (value >= 0) 
+            {
+                _balance = value;
+            }
+            else
+            {
+                Console.WriteLine("Balance cannot be negative."); 
+            }
+        }
+    }
+
+
+
+}
---
+++ Encapsualtion/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using Encapsulation;
+namespace Encapsulation;
+
+public class Program : Product
+{
+    public Program(string brandName, string model, int price, int cost, int count)
+        : base(brandName, model, price, cost, count)
+    {
+    }
+
+    static void Main(string[] args)
+    {
+
+        Product product = new Product("apple", "iphone", 999, 600, 17);
+
+
+        Console.WriteLine(string.Concat("ID: ", product.IdMaker(product.BrandName, product.Model)));
+
+
+        product.GetInfo();
+
+
+        (var updatedCount, var updatedIncome) = product.Sale(product.Income, product.Price, product.Cost, product.Count, 3);
+
+
+        product.Count = updatedCount;
+        product.Income = updatedIncome;
+
+
+        Console.WriteLine($"Updated Income: {updatedIncome}, Updated Count: {updatedCount}");
+        Phone phone = new Phone("apple", "iphone", 999, 500, 13, 8, "red", 1.2m);
+        Console.WriteLine(phone.Call(6, phone.Balance));
+
+        Console.ReadLine();
+    }
+}
